f5666f1f9e275fee8cd10366133be8dd62f33fea
makarik1 <irinka999@list.ru>
2022-08-08 00:01:20 +0300

    Add tests for LoginPage

---
+++ test_login_page.py
@@ -0,0 +1,21 @@
+from .pages.login_page import LoginPage
+
+from selenium.webdriver.common.by import By
+
+def test_guest_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_be_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
