6e68db278fb0ba7a32c6b4018bf76cddd42a99a6
makarik1 <irinka999@list.ru>
2022-08-07 22:30:47 +0300

    Add base_pade.py and main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,19 @@
 from selenium.webdriver.common.by import By
 
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    go_to_login_page(browser)
+
+"""
+def test_add_to_cart(browser):
+    page = ProductPage(url="", browser)   # инициализируем объект Page Object
+    page.open()                           # открываем страницу в браузере
+    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
+    page.add_product_to_cart()            # жмем кнопку добавить в корзину
+    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
+"""
